3ddd6c174fb7c73b367f95f20ffc8e6b0afad804
RileyAlexis <MxRileyAlexis@gmail.com>
2024-08-11 13:30:13 -0500

    API data formatting

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,23 @@
 import express from 'express';
 import { Router } from "express";
+const bodyParser = require('body-parser');
 
-const app = express();
-const port = 5001;
+//Types
+import { Express } from 'express';
+import { Request, Response } from 'express';
 
-app.get('/', (req, res) => {
+//Routes
+const bathroomRequest = require('./Routes/bathroomRequest');
+
+const app: Express = express();
+const port: number = 5001;
+
+app.get('/', (req: Request, res: Response) => {
     res.send('This thing is working get route /');
     console.log('Route called')
 });
+
+app.use('/api', bathroomRequest);
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server Running at http://0.0.0.0:${port}`);
